330f53e18d0bec45f5917958a5c8782903aa62f0
Asjedh02 <m.asjedhcr7@gmail.com>
2024-02-06 20:56:59 +0530

    first commit

---
+++ src/components/UserLogin.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { RiAdminFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+const UserLogin = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center h-[100px] w-full bg-blue-100 ">
+        <div className="p-5 font-signature text-3xl underline underline-offset-4">
+          TaskFlowHub
+        </div>
+        <div className="flex p-5 gap-10">
+          <Link
+            className="bg-blue-500 px-4 py-1 text-lg font-bold rounded-lg hover:underline underline-offset-4 flex items-center"
+            to={"/login"}
+          >
+            <span>Login</span>
+          </Link>
+          <Link
+            className="bg-blue-500 px-4 py-1 text-lg font-bold rounded-lg hover:underline underline-offset-4 flex items-center"
+            to={"/register"}
+          >
+            <span>Register</span>
+          </Link>
+          <Link
+            className="bg-blue-500 px-4 py-1 text-lg font-bold rounded-lg flex flex-col items-center justify-center "
+            to={"/admin_login"}
+          >
+            <span>Admin Login</span>
+            <RiAdminFill size={20} />
+          </Link>
+        </div>
+      </div>
+      <div className="h-[800px] flex items-center justify-center">
+        <div className="p-20 bg-gray-300 rounded-lg shadow-md border-4 border-black">
+          <h1 className="flex justify-center items-center text-xl font-signature ">
+            Login to your account
+          </h1>
+          <form className="mt-5 flex flex-col">
+            <label className="font-semibold" htmlFor="username">
+              Email:
+            </label>
+            <input
+              className="rounded-lg py-2 px-3 mb-6 border-2 border-black "
+              type="email"
+            ></input>
+            <label className="font-semibold" htmlFor="password">
+              Password:
+            </label>
+            <input
+              className="rounded-lg py-2 px-3 mb-6 border-2 border-black"
+              type="password"
+            ></input>
+            <button className="m-8 bg-blue-400 rounded-xl py-2 hover:bg-blue-600 text-white border-2 border-black ">
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogin;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import UserLogin from "./components/UserLogin";
+import AdminLogin from "./components/AdminLogin";
+import UserRegister from "./components/UserRegister";
+import UserDash from "./components/UserDash";
+import AdminDash from "./components/AdminDash";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <UserLogin />,
+      },
+      {
+        path: "/admin_login",
+        element: <AdminLogin />,
+      },
+      {
+        path: "/register",
+        element: <UserRegister />,
+      },
+      {
+        path: "/user_dashboard",
+        element: <UserDash />,
+      },
+      {
+        path: "/admin_dashboard",
+        element: <AdminDash />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
